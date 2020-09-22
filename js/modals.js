a19b57a517c4cb11164bbec8f67501f5fdea6c3b
Chloe-O <62480693+Chloe-O@users.noreply.github.com>
2020-09-22 22:08:00 +0100

    modal overlay added

--- js/modals.js
+++ js/modals.js
@@ -1,17 +1,21 @@
 let modal = document.getElementById("myModal");
+let modalOverlay = document.getElementById("modal-overlay");
 let cardButton = document.getElementById("cardBtn");
 let span = document.getElementsByClassName("close")[0];
 
 cardButton.onclick = function () {
   modal.style.display = "block";
+  modalOverlay.style.display = "block";
 };
 
 span.onclick = function () {
   modal.style.display = "none";
+  modalOverlay.style.display = "none";
 };
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
+    modalOverlay.style.display = "none";
   }
 };
