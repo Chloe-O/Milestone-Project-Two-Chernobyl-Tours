aa32d443906fa25dc5b0ca6e2270a6ff2f79547f
Chloe-O <62480693+Chloe-O@users.noreply.github.com>
2020-09-22 20:59:48 +0100

    card-image problem resolved

---
+++ js/modals.js
@@ -0,0 +1,7 @@
+let modal = document.getElementById("myModal");
+let cardButton = document.getElementById("cardBtn");
+let span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+};
