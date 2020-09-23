a5fcaac293e5a92dd1dc8ba1292846a66f2e6ca3
Chloe-O <62480693+Chloe-O@users.noreply.github.com>
2020-09-23 07:39:31 +0100

    links added to buttons

--- js/modals.js
+++ js/modals.js
@@ -1,3 +1,5 @@
+/*Reactor 4*/
+
 let reactorModal = document.getElementById("vidModal");
 let modalOverlay = document.getElementById("modal-overlay");
 let cardButton = document.getElementById("reactorButton");
@@ -12,3 +14,29 @@ span.onclick = function () {
   reactorModal.style.display = "none";
   modalOverlay.style.display = "none";
 };
+
+/*Amusement Park*/
+
+let amusementButton = document.getElementById("amuseButton");
+
+amusementButton.onclick = function () {
+  window.open(
+    "https://www.tripadvisor.co.uk/Attraction_Review-g10441492-d7087958-Reviews-Pripyat_Amusement_Park-Pripyat_Kiev_Oblast.html"
+  );
+};
+
+/*Duga radar*/
+
+let dugaButton = document.getElementById("dugaButton");
+
+dugaButton.onclick = function () {
+  window.open("https://en.wikipedia.org/wiki/Duga_radar#");
+};
+
+/*Red forest*/
+
+let forestButton = document.getElementById("forestButton");
+
+forestButton.onclick = function() {
+    window.open("")
+}
